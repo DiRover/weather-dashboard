df2567220107a58be9f173fe391423850b3226ef
Dmitrij Rovenskij <rovenskij@element-soft.com>
2025-08-21 11:25:19 +0300

    user form fix

--- src/entities/users/UserForm.tsx
+++ src/entities/users/UserForm.tsx
@@ -17,27 +17,44 @@ interface UserFormProps {
 const {Password} = Input;
 
 const UserForm = memo(({onCreated}: UserFormProps) => {
+    const [form] = Form.useForm();
+
     const handleCreateUser = useCallback(
         (values: UserFormValues) => {
             const {username, password} = values;
             createUser({username: username.trim(), password});
+            form.resetFields();
             onCreated();
         },
-        [onCreated],
+        [onCreated, form],
     );
 
     return (
-        <Form onFinish={handleCreateUser} className="flex flex-col">
+        <Form form={form} onFinish={handleCreateUser} className="flex flex-col">
             <Item
                 name="username"
-                rules={[{required: true, message: 'Введите логин'}, {min: 3}]}
+                rules={[
+                    {required: true, message: 'Введите логин'},
+                    {
+                        min: 3,
+                        message:
+                            'Имя пользователя должно быть больше или равно 3 символам',
+                    },
+                ]}
             >
                 <Input autoComplete="username" placeholder="Логин" allowClear />
             </Item>
 
             <Item
                 name="password"
-                rules={[{required: true, message: 'Введите пароль'}, {min: 3}]}
+                rules={[
+                    {required: true, message: 'Введите пароль'},
+                    {
+                        min: 3,
+                        message:
+                            'Пароль должен быть больше или равен 3 символам',
+                    },
+                ]}
             >
                 <Password
                     autoComplete="new-password"
