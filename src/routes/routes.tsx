2afff665fdf9e27a19f4132b2061db4c43de3696
Dmitrij Rovenskij <rovenskij@element-soft.com>
2025-08-22 11:50:29 +0300

    error page file move to folder

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import type {RouteObject} from 'react-router';
 
 import type {RouteFull} from '@/routes/types.ts';
 
-import ErrorPage from '@/entities/ErrorPage.tsx';
+import ErrorPage from '@/entities/error/ErrorPage.tsx';
 import {adminSection} from '@/routes/adminSection.tsx';
 import {weatherGraphSection} from '@/routes/weatherGraphSection.tsx';
 
